64e037e8646009267a07c2bb2787f40300c4812f
DoThuyLinh <dothuylinh2105@gmail.com>
2020-05-11 01:05:06 +0700

    btth tuan 1

---
+++ tuan1/TCPServer/TcpClient/Form1.cs
@@ -0,0 +1,49 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TcpClient
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+        SimpleTcpClient tcpClient;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tcpClient = new SimpleTcpClient();
+            tcpClient.StringEncoder = Encoding.UTF8;
+            tcpClient.DataReceived += TcpClient_DataReceived;
+        }
+
+        private void TcpClient_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            txtStatus.Invoke((MethodInvoker)delegate ()
+            {
+                txtStatus.Text += e.MessageString;
+
+            });
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            IPAddress ip = IPAddress.Parse(txtHost.Text);
+            tcpClient.Connect(txtHost.Text, Convert.ToInt32(txtPort.Text));
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            tcpClient.WriteLineAndGetReply(txtMessage.Text, TimeSpan.FromSeconds(3));
+        }
+    }
+}
---
+++ tuan1/TCPServer/TcpServer/Form1.cs
@@ -0,0 +1,94 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TcpServer
+{
+    public partial class Server : Form
+    {
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer tcpServer;
+        private TcpClient _connectedClient = null;
+        const int MAX_CONNECTION = 10;
+
+        private void Server_Load(object sender, EventArgs e)
+        {
+            tcpServer = new SimpleTcpServer();
+            tcpServer.Delimiter = 0x13;
+            tcpServer.StringEncoder = Encoding.UTF8;
+
+        }
+
+        private void TcpServer_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            txtStatus.Invoke((MethodInvoker)delegate ()
+            {
+                txtStatus.Clear();
+                txtStatus.Text += "Client"+((System.Net.IPEndPoint)e.TcpClient.Client.RemoteEndPoint).Address.ToString()+" send: " + e.MessageString;
+                e.ReplyLine(string.Format("Server send: {0}", e.MessageString + "\n"));
+            });
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtStatus.Text += "Server starting...";
+            IPAddress ip = IPAddress.Any;
+            tcpServer.Start(ip, Convert.ToInt32(txtPort.Text));
+            LoopClients();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (tcpServer.IsStarted)
+                tcpServer.Stop();
+
+        }
+
+        public void LoopClients()
+        {
+            for (int i = 0; i < MAX_CONNECTION; i++)
+            {
+                new Thread(HandleClient).Start();
+            }
+            //while (_connectionsCount < MAX_CONNECTION)
+            //{
+            //    tcpServer.ClientConnected += TcpServer_ClientConnected;
+            //    _connectionsCount++;
+            //    Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
+            //    t.Start(_connectedClient);
+            //}
+        }
+        
+        private void TcpServer_ClientConnected(object sender, TcpClient e)
+        {
+            if (_connectedClient == null)
+            {
+                _connectedClient = e;
+            }
+            else
+            {
+                e.Dispose();
+            }
+        }
+
+        public void HandleClient(object obj)
+        {
+            tcpServer.ClientConnected += TcpServer_ClientConnected;
+            tcpServer.DataReceived += TcpServer_DataReceived;
+        }
+    }
+}
